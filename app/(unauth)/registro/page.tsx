d24e9e03d3fb9f90df0ca6cc570ddd60f38d8fc2
Wilson Furtado <furtadorodrigo88@gmail.com>
2026-05-04 12:35:46 -0100

    criar pagina de registro

---
+++ app/(unauth)/registro/page.tsx
@@ -0,0 +1,13 @@
+import { LeftSection } from "@/components/registro/left-section";
+import { RightSection } from "@/components/registro/right-section";
+
+
+export default function RegistroPage() {
+    return (
+        <div className="bg-gray-200 h-screen flex justify-between">
+            <LeftSection/>
+
+            <RightSection/>
+        </div>
+    )
+}
