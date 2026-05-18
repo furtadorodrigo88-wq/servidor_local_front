0e6087f6e1088670321dee84aa2af118a77ad3e8
Wilson Furtado <furtadorodrigo88@gmail.com>
2026-05-18 11:53:36 -0100

    ...

--- app/(auth)/home/page.tsx
+++ app/(auth)/home/page.tsx
@@ -1,34 +1,35 @@
 "use client";
-import { PedidoCard } from "@/components/core/pedido-card"
-import { Header } from "@/components/core/header"
-import { useAllPrestacoesServicos } from "@/app/data/queries/prestacao-servico";
+import { PedidoCard } from "@/components/core/pedido-card";
+import { getAllPrestacoesServicos } from "@/data/queries/prestacao-servico";
+import { parseCookies } from "nookies";
 
+export default function HomePage() {
+  const { loading, error, data } = getAllPrestacoesServicos();
 
-export default function Home() {
-    const { loading, error, data } = useAllPrestacoesServicos();
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
-    return (
-        <div className="bg-gray-200">
-            <Header />
-            <h1>Home</h1>
-            <div className="w-full grid grid-cols-3 gap-6">
-                {data?.getAllServiceProv?.map((prestacaoServico) => (
-                    <PedidoCard
-                        key={prestacaoServico.id}
-                        id={prestacaoServico.id}
-                        title={prestacaoServico.disign}
-                        desciption={prestacaoServico.disign}
-                        image="/placeholder.jpeg"
-                        category={{
-                            id: prestacaoServico.id,
-                            nome: "Categoria",
-                            icone: "/file.svg",
-                        }}
-                    />
-                ))}
-            </div>
-        </div>
-    )
+  const { user, token } = parseCookies();
+
+  const userData = JSON.parse(user);
+
+  const orcaamentoPayload = {
+    id_utilizador: userData.id,
+  };
+
+  return (
+    <div>
+      <h1>Home</h1>
+      {data?.map((prestacaoServico) => (
+        <PedidoCard
+          key={prestacaoServico.id}
+          id={prestacaoServico.id}
+          title={prestacaoServico.designacao}
+          desciption={prestacaoServico.designacao}
+          image="/placeholder.jpeg"
+          category={prestacaoServico.servico.categoria}
+        />
+      ))}
+    </div>
+  );
 }
